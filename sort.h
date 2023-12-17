b4ee08a9fa4b2bd6cadb38ee1ce798d02c913a2d
oumaima <oumaimanaanaapro@gmail.com>
2023-12-17 21:45:19 +0100

    betty header

--- sort.h
+++ sort.h
@@ -19,10 +19,10 @@ typedef struct listint_s
 } listint_t;
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -34,8 +34,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -47,9 +47,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 void print_array(const int *array, size_t size);
