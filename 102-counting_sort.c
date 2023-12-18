edae91459781c3ec343f33b280fb7d09e839116f
Badr_Annabi <badrannabi@gmail.com>
2023-12-18 21:58:02 +0100

    counting sort with O

---
+++ 102-counting_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * counting_sort - sorts an array of integers in ascending
+ * order using the Counting sort algorithm
+ * @array: Our array.
+ * @size: size of the array.
+ */
+void counting_sort(int *array, size_t size)
+{
+	int j, *count, *output, max = array[0];
+	size_t i;
+
+	if (!array || size < 2)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	count = malloc(sizeof(int) * (max + 1));
+	if (!count)
+	{
+		free(count);
+		return;
+	}
+	for (j = 0; j <= max; j++)
+		count[j] = 0;
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+	for (j = 1; j <= max; j++)
+		count[j] += count[j - 1];
+	print_array(count, max + 1);
+	output = malloc(sizeof(int) * size);
+	if (!output)
+	{
+		free(count);
+		return;
+	}
+	for (i = size - 1; i < size; i--)
+	{
+		output[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
+	}
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+	free(count);
+	free(output);
+}
