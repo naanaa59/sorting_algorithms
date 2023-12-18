bf2e1641b37792f536ccf811671624d327f37b2c
Badr_Annabi <badrannabi@gmail.com>
2023-12-18 23:06:49 +0100

    Counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -11,7 +11,7 @@ void counting_sort(int *array, size_t size)
 	int j, *count, *output, max = array[0];
 	size_t i;
 
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size; i++)
 	{
