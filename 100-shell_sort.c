044f5cd812e35d7dc560a1a1aa1afc9cbc6e8390
Badr_Annabi <badrannabi@gmail.com>
2023-12-18 01:33:52 +0100

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -6,33 +6,33 @@
  * Knuth sequence
  * @array: our array
  * @size: array's size
-*/
+ */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i, j;
-    int tmp;
+	size_t gap = 1, i, j;
+	int tmp;
 
-    while (gap < size / 3)
-    {
-        gap = (gap * 3) + 1;
-    }
+	while (gap < size / 3)
+	{
+		gap = (gap * 3) + 1;
+	}
 
-    while (gap > 0)
-    {
-        for (i = gap; i < size; i++)
-        {
-            tmp = array[i];
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			tmp = array[i];
 
-            for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
-            {
-                array[j] = array[j - gap];
-            }
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
 
-            array[j] = tmp;
-        }
-        print_array(array, size);
-        gap /= 3;
-    }
-    
+			array[j] = tmp;
+		}
+		print_array(array, size);
+		gap /= 3;
+	}
+
 
 }
