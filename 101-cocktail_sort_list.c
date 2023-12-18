2ff231e1f5709538a7a4f2ca3bcd90e0a75c637c
Badr_Annabi <badrannabi@gmail.com>
2023-12-18 17:09:20 +0100

    cocktail sort with big O

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,75 @@
+#include "sort.h"
+/**
+ * swap_nodes - swaps nodes.
+ * @list: the header of the list.
+ * @node1: first node.
+ * @node2: second node.
+ */
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	if (node1->prev)
+		node1->prev->next = node2;
+	else
+		*list = node2;
+	if (node2->next)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+
+	node1->prev = node2;
+	node2->next = node1;
+}
+
+/**
+ * cocktail_sort_list - sorts a doubly linked list
+ * of integers in ascending order using the Cocktail
+ * shaker sort algorithm.
+ * @list: Doubly linked list.
+ */
+void cocktail_sort_list(listint_t **list)
+{
+	int swapped;
+	listint_t *current;
+
+	if (!list || !(*list) || !((*list)->next))
+		return;
+
+	do
+	{
+		swapped = 0;
+		current = *list;
+
+		while(current->next)
+		{
+			if (current->n > current->next->n)
+			{
+				swap_nodes(list, current, current->next);
+				swapped = 1;
+				print_list(*list);
+			}
+			else
+				current = current->next;
+		}
+
+		if (!swapped)
+			break;
+
+		swapped = 0;
+
+		while(current->prev)
+		{
+			if (current->n < current->prev->n)
+			{
+				swap_nodes(list, current->prev, current);
+				swapped = 1;
+				print_list(*list);
+			}
+			else
+				current = current->prev;
+		}
+
+	} while (swapped);
+
+
+}
