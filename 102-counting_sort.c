af663f2857779a7825b23a505270cf6cf30b3340
Badr_Annabi <badrannabi@gmail.com>
2023-12-19 21:37:21 +0100

    counting sort last update

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,115 +1,104 @@
 #include "sort.h"
+
 /**
  * counting_sort - sorts an array of integers in ascending order
- * using the Counting sort algorithm
- * @array: array to sort
- * @size: size of the array
+ * using the Counting sort algorithm.
+ * @array: our array to sort.
+ * @size: size of the array.
  */
 void counting_sort(int *array, size_t size)
 {
 	size_t i;
-	int *counting_array, *sorted_array;
+	int *count_arr, *output_arr;
 
 	if (!array || size < 2)
 		return;
-	counting_array = create_counting_array(array, size);
-	if (!counting_array)
+	count_arr = count(array, size);
+	if (!count_arr)
 		return;
-	sorted_array = create_sorted_array(array, size, counting_array);
-	if (!sorted_array)
+	output_arr = output(array, size, count_arr);
+	if (!output_arr)
 	{
-		free(counting_array);
+		free(count_arr);
 		return;
 	}
-	free(counting_array);
+	free(count_arr);
 	i = 0;
 	while (i < size)
 	{
-		array[i] = sorted_array[i];
+		array[i] = output_arr[i];
 		i++;
 	}
-	free(sorted_array);
+	free(output_arr);
 }
 /**
- * create_sorted_array - Creates sorted array for
- * Counting sort Algo
- * @array: array of integers.
- * @size: size of the array.
- * @counting_array: array of cumulative occurences.
- *
- * Return: Sorted array for Counting Sort algo.
+ * get_max - Searches for max in array.
+ * @array: Our array.
+ * @size: size of array.
+ * Return: maximum element in the array.
  */
-int *create_sorted_array(int *array, size_t size, int *counting_array)
+int get_max(int *array, size_t size)
 {
-	int *sorted_array;
+	int max = array[0];
 	size_t i;
 
-	sorted_array = malloc(size * sizeof(int));
-	if (!sorted_array)
-		return (0);
-
-	i = size - 1;
-	while (1)
-	{
-		sorted_array[counting_array[array[i]] - 1] = array[i];
-		counting_array[array[i]]--;
-		if (!i--)
-			break;
-	}
-
-	return (sorted_array);
-}
-/**
- * create_counting_array - Creates counting array for
- * Counting sort Algo
- * @array: array of integers.
- * @size: size of the array.
- *
- * Return: counting array for Counting Sort algo.
- */
-int *create_counting_array(int *array, size_t size)
-{
-	int *counting_array;
-	size_t i, counting_array_len;
-
-	counting_array_len = array_max(array, size) + 1;
-	counting_array = malloc(counting_array_len * sizeof(int));
-	if (!counting_array)
-		return (0);
-
-	i = 0;
-	while (i < counting_array_len)
-		counting_array[i++] = 0;
-
-	i = 0;
-	while (i < size)
-		counting_array[array[i++]]++;
-
-	i = 0;
-	while (++i < counting_array_len)
-		counting_array[i] += counting_array[i - 1];
-	print_array(counting_array, counting_array_len);
-	return (counting_array);
-}
-/**
- * array_max - Searches for max in array.
- * @array: array to search in.
- * @size: size of @array.
- *
- * Return: max element of @array
- */
-int array_max(int *array, size_t size)
-{
-	int max;
-	size_t i;
-
-	i = 0;
-	max = array[0];
-	while (i < size)
+	for (i = 0; i < size; i++)
 	{
 		if (array[i] > max)
 			max = array[i];
-		++i;
 	}
 	return (max);
+}
+/**
+ * output - Creates a sorted array.
+ * @array: Our array
+ * @size: Size of the array.
+ * @count: Array of coumpounded elements.
+ * Return: sorted array.
+ */
+int *output(int *array, size_t size, int *count)
+{
+	int *output;
+	size_t j;
+
+	output = malloc(size * sizeof(int));
+	if (!output)
+		return (0);
+	j = size - 1;
+	while (1)
+	{
+		output[count[array[j]] - 1] = array[j];
+		count[array[j]]--;
+		if (!j--)
+			break;
+	}
+	return (output);
+}
+/**
+ * count - Creates counting array to count
+ * the elements or the array.
+ * @array: array of integers.
+ * @size: size of the array.
+ * Return: counting array.
+ */
+int *count(int *array, size_t size)
+{
+	int *count;
+	size_t i, count_len;
+
+	count_len = get_max(array, size) + 1;
+	count = malloc(count_len * sizeof(int));
+	if (!count)
+		return (0);
+	i = 0;
+	while (i < count_len)
+		count[i++] = 0;
+	i = 0;
+	while (i < size)
+		count[array[i++]]++;
+	i = 0;
+	while (++i < count_len)
+		count[i] += count[i - 1];
+	print_array(count, count_len);
+	return (count);
 }
